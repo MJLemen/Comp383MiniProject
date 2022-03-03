348abf5edeba9835294fc190337b7917b0b72c6b
MJLemen <mlemenager@luc.edu>
2022-03-02 23:10:28 -0600

    added comments to mp1.py

--- mp1.py
+++ mp1.py
@@ -62,24 +62,22 @@ with open(fileName, 'w') as log: #open the file
 	fasta_dict = read_fasta(sra_assembly_path+'contigs.fasta') #find the contigs file and read the fasta file
 	fasta_dict_keys = fasta_dict.keys() #get the keys of the dict
 	contigsAbove1000 = 0 #initialize counter for # of contigs above thousand
-	contigList = [] #list of keys for contigs above 1000
 	total_bp_contigs_over_1000 = 0 #counter for total bp
-	contigdict1000 = {}	
+	contigdict1000 = {} #dictionary for contigs over 1000 bp	
 
-	for i in fasta_dict_keys:
-		headerList = i.split('_')
-		length = int(headerList[3])
-		if length > 1000:
-			contigsAbove1000 +=1
-			contigList.append(i)
-			total_bp_contigs_over_1000 += length
-			contigdict1000[i] = fasta_dict[i]
+	for i in fasta_dict_keys: #run through all keys of contig dict
+		headerList = i.split('_') #take header b/c has length value and split into list
+		length = int(headerList[3]) #4th item is always the length
+		if length > 1000: #if greater than 1000
+			contigsAbove1000 +=1 #adds to contig counter
+			total_bp_contigs_over_1000 += length #adds to total bases counter
+			contigdict1000[i] = fasta_dict[i] #adds sequence and key to another dictionary
 
-	with open(path+'/1000_contigs.fasta','w') as out:
+	with open(path+'/1000_contigs.fasta','w') as out: #write out the larger sequences to a separate file to be used for genemark
 		for i in contigdict1000.keys():
 			out.write( i + '\n')
 			out.write(str(contigdict1000[i])+'\n')
-	contig_path = path + '/1000_contigs.fasta'			
+	contig_path = path + '/1000_contigs.fasta' #make file path variable
 	
 	log.write('There are ' + str(contigsAbove1000) + ' contigs > 1000 in the assembly.\n')
 
@@ -100,25 +98,26 @@ with open(fileName, 'w') as log: #open the file
 
 ###PROBLEM 6###
 
-	query_file = predicted_cds_file
-	outfmt = '"10 qseqid sseqid pident qcovs"'
-	db_file = 'Ecoli.fasta'
-	output_file = path + '/predicted_functionality.csv'
-	makedb = 'makeblastdb -in ' + db_file  + '-out ' + path + '/Ecoli -title ' + path + '/Ecoli -dbtype prot'
-	db_path = path + '/Ecoli'
-	#os.system(makedb)
+	query_file = predicted_cds_file #create query file variable
+	outfmt = '"10 qseqid sseqid pident qcovs"' #out format options
+	db_file = 'Ecoli.fasta' #database file being used
+	output_file = path + '/predicted_functionality.csv' #output file
+	makedb = 'makeblastdb -in ' + db_file  + '-out ' + path + '/Ecoli -title ' + path + '/Ecoli -dbtype prot' #make db command
+	db_path = path + '/Ecoli' #db path for blast command
+	#os.system(makedb) #make db call
 	blast_command='blastp -query '+ query_file + ' -db ' + db_path + ' -max_target_seqs 1 -out ' + output_file + ' -outfmt ' + outfmt 
-	#os.system(blast_command)
+	#os.system(blast_command) #blast command and call
 	#blast the contigs file from genemark output against the Ecoli database and output to predicted_functionality.csv file in results folder
 
 ###PROBLEM 7###	
 	with open(output_file, 'r') as blast:
 		blast_cds = blast.readlines() #blast comparisons
 		num_cds = len(blast_cds)
-	
-	refseq_Ecoli_cds = 4140
-	discrepancy = 0
-	if num_cds > refseq_Ecoli_cds:
+	#open the output file from the blast command
+	#and get the total amount of cds (each line is a separate CDS)
+	refseq_Ecoli_cds = 4140 #total refseq CDS for the SRR number
+	discrepancy = 0 #variable to calculate
+	if num_cds > refseq_Ecoli_cds: #write out the discrepancy b/w genmark and refseq
 		discrepancy = num_cds - refseq_Ecoli_cds
 		log.write('GeneMarkS found ' + str(discrepancy) + ' additional CDS than the RefSeq.\n')
 	elif num_cds < refseq_Ecoli_cds:
